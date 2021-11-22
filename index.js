cc945374ea38423b68a4935597656d29bdd26550
thanhson1990 <59007114+thanhson1990@users.noreply.github.com>
2021-11-22 14:13:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    var war = $("#warning");
+    var warning = null;
+    var isClicked = false;
+    var cl = null;
+    var bar = $("#pb");
+    //war.show();
 
     function showMes(percent) {
 
@@ -25,50 +31,63 @@ $(document).ready(function() {
         } else if (percent == 100) {
 
             var text = "Hacked NASA. Congratulations !!!";
+            war.text("Unauthorized Access !!!");
             $("#done").text(text);
+            $("#clk").text("Click to Hack again !");
+            isClicked = false;
+            clearInterval(cl);
         }
-
-
     }
 
     function clearMes() {
         $("#mes").children().text("");
-    }
-    var flag = true;
-    var cl = null;
-    var bar = $("#pb");
-    $("#clk").click(function() {
         clearInterval(cl);
+        war.text("");
         bar.css("width", 0 + "%");
         bar.text("");
-        clearMes();
-        ////
-        var width = 0;
-        cl = setInterval(function() {
-            var rd = Math.floor(Math.random() * 11);
-            // var w = "";
-            width += rd;
-            console.log(width);
-            if (width > 100) {
-                width = 100;
-                bar.css("width", width + "%");
-                bar.text(width + "% Completed");
-                showMes(width);
-            } else {
-                //  w = width + "%";
-                bar.css("width", width + "%");
-                bar.text(width + "% Completed");
-                showMes(width);
-            }
+    }
 
-        }, 1000);
+    $("#clk").click(function() {
+        if (isClicked) { // STOP
+            isClicked = false;
+            $("#clk").text("Click to Hack !");
+            clearMes();
+        } else { // HACK
+            clearMes()
+            $("#clk").text("End Task !");
+            //war.show();
+            isClicked = true;
+            var width = 0;
+            cl = setInterval(function() {
+                var rd = Math.floor(Math.random() * 11);
+                // var w = "";
+                width += rd;
+                console.log(width);
+                if (width > 100) {
+                    width = 100;
+                    bar.css("width", width + "%");
+                    bar.text(width + "% Completed");
+                    showMes(width);
+                } else {
+                    //  w = width + "%";
+                    bar.css("width", width + "%");
+                    bar.text(width + "% Completed");
+                    showMes(width);
+                }
+
+            }, 1000);
+        }
+
+        ////
+
 
     });
+    /*
     $("#stop").click(function() {
         clearInterval(cl);
         bar.css("width", 0 + "%");
         bar.text("");
         clearMes();
     });
-
+*/
 });
